e61b080239503d09034b1144eb10fd3b39383aee
simba <simbamuzika@gmail.com>
2025-07-02 12:35:57 +0200

    added oakservice

---
+++ oakmicroservices/src/main/java/com/example/oakmicroservices/OakmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.oakmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakmicroservicesApplication.class, args);
+	}
+
+}
