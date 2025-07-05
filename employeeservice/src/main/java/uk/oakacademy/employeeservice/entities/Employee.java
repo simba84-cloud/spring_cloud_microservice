960f2e43db1e46a4cb661abc4b12f780a93f294a
simba <simbamuzika@gmail.com>
2025-07-05 10:31:18 +0200

    added validation and fields

--- employeeservice/src/main/java/uk/oakacademy/employeeservice/entities/Employee.java
+++ employeeservice/src/main/java/uk/oakacademy/employeeservice/entities/Employee.java
@@ -1,11 +1,15 @@
 package uk.oakacademy.employeeservice.entities;
 
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Document(collection="employees")
 @Getter
@@ -15,10 +19,29 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Employee {
     @Id
     private String id;
+    @NotBlank
+    @NotEmpty
     private String name;
     private String surname;
+    @Size(min = 10, max = 50)
+    @Email
+    @Pattern(regexp = "[a-zA-z0-9]")
     private String email;
     private String position;
+    @Min(10)
+    @Max(100)
+    //@Pattern(regexp = "[0-9]")
+    //@DecimalMin()
+    //@DecimalMax()
+    //@Positive
+    @PositiveOrZero
+//    @Negative
+//    @NegativeOrZero
+    private int age;
+//    @Future
+//    @Past
+    @CreationTimestamp
+    private Date addedDate;
 
 
 }
